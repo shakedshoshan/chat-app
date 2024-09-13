6acfe1ccddd261dfbe2a73de6cacd9b7bf1ccc45
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-13 17:47:03 +0300

    finish backend

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,54 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+        participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+        conversation = await Conversation.create({
+            participants: [senderId, receiverId],
+        });
+    }
+
+    const newMessage = new Message({
+        senderId,
+        receiverId,
+        message,
+    });
+
+    if (newMessage) {
+        conversation.messages.push(newMessage._id);
+    }
+
+    res.status(200).json(newMessage);
+
+    // this will run in parallel
+		await Promise.all([conversation.save(), newMessage.save()]);
+
+}
+
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+		if (!conversation) return res.status(200).json([]);
+
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
