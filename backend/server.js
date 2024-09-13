ec39f3fe73c6ea1154b4b2e57dbccc32f7c6d124
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-13 16:04:19 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! with nodemon');
+});
+
+app.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
