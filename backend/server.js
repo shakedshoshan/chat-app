a9470ec709fb1105c321af55a6bf0772dcfd25f7
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-15 17:31:16 +0300

    deployment update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,10 +25,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World! with nodemon');
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
