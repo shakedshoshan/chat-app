a77ecf4d7162682a00439cf095734e39fae5510c
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-15 13:24:11 +0300

    handle massages functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,37 +1,36 @@
-// import { useEffect } from "react";
-// import useConversation from "../../zustand/useConversation";
-// import MessageInput from "./MessageInput";
-// import Messages from "./Messages";
-// import { TiMessages } from "react-icons/ti";
-// import { useAuthContext } from "../../context/AuthContext";
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
-// const MessageContainer = () => {
-// 	const { selectedConversation, setSelectedConversation } = useConversation();
+const MessageContainer = () => {
+	const { selectedConversation, setSelectedConversation } = useConversation();
 
-// 	useEffect(() => {
-// 		// cleanup function (unmounts)
-// 		return () => setSelectedConversation(null);
-// 	}, [setSelectedConversation]);
+	useEffect(() => {
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
 
-// 	return (
-// 		<div className='md:min-w-[450px] flex flex-col'>
-// 			{!selectedConversation ? (
-// 				<NoChatSelected />
-// 			) : (
-// 				<>
-// 					{/* Header */}
-// 					<div className='bg-slate-500 px-4 py-2 mb-2'>
-// 						<span className='label-text'>To:</span>{" "}
-// 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-// 					</div>
-// 					<Messages />
-// 					<MessageInput />
-// 				</>
-// 			)}
-// 		</div>
-// 	);
-// };
-// export default MessageContainer;
+	return (
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
+};
+export default MessageContainer;
 
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
@@ -46,23 +45,3 @@ const NoChatSelected = () => {
 	);
 };
 
-// STARTER CODE SNIPPET
-import MessageInput from "./MessageInput";
-import Messages from "./Messages";
-
-const MessageContainer = () => {
-	return (
-		<div className='md:min-w-[450px] flex flex-col'>
-			<>
-				{/* Header */}
-				<div className='bg-slate-500 px-4 py-2 mb-2'>
-					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-				</div>
-
-				<Messages />
-				<MessageInput />
-			</>
-		</div>
-	);
-};
-export default MessageContainer;
